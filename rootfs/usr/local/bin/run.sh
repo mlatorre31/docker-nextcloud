9b640a84ed780f373bd0854ce02d63df6abe5d3f
Ben Yanke <ben@benyanke.com>
2020-10-11 23:05:48 -0500

    updating logging functions in entrypoint

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -39,25 +39,43 @@ echo "Done updating permissions."
 
 if [ ! -f /config/config.php ]; then
     # New installation, run the setup
+    echo
+    echo "Detecting new install - generating config"
+    echo
     /usr/local/bin/setup.sh
 else
     # Run upgrade if applicable
+    echo
+    echo "Running occ upgrade"
     occ upgrade
 
     # Add missing indexes
+    echo
+    echo "Running occ db:add-missing-indices"
     occ db:add-missing-indices
 
     # Convert filecache fields
+    echo
+    echo "Running occ db:convert-filecache-bigint"
     occ db:convert-filecache-bigint
 
     # Update DB schema as needed
+    echo
+    echo "Running occ db:convert-mysql-charset"
     occ db:convert-mysql-charset
 
 fi
 
 # Run auto update
 if [ "$APP_AUTO_UPDATE" = "1" ] ; then
-  occ app:update --all
+    echo
+    echo "Running occ app:update --all"
+    occ app:update --all
 fi
 
+echo
+echo "Provisioning complete - starting app"
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
+echo
+echo "App exiting now"
+echo
