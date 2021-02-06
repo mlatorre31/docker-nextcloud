9ec44853d46f5cfa35ca5ec9136d6e35af497310
mlatorre31 <server@latorre.pw>
2021-02-07 00:37:54 +0100

    Catchup with Benyanke

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -18,17 +18,16 @@ if [ ! -d /data/session ]; then
   mkdir -p /data/session;
 fi
 
-# These dirs always are reset
-permDirs="/nextcloud /config /apps2 /var/log /php /nginx /tmp /etc/s6.d"
-
 if [ "$PERMISSION_RESET" = "1" ] ; then
-  permDirs="$permDirs /data"
+  export dirsToFix="/nextcloud /data /config /apps2 /var/log /php /nginx /tmp /etc/s6.d"
 else
-  echo "Not updating permissions on /data since \$PERMISSION_RESET was not '1'";
+  export dirsToFix="/nextcloud /config /apps2 /var/log /php /nginx /tmp /etc/s6.d"
+  echo "Not updating permissions on /data directory since \$PERMISSION_RESET was not '1'";
 fi
 
 echo "Updating permissions..."
-for dir in $permDirs; do
+for dir in $dirsToFix; do
+  echo "Handling permissions in $dir"
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
     echo "Updating permissions in $dir..."
     chown -R $UID:$GID $dir
@@ -38,28 +37,45 @@ for dir in $permDirs; do
 done
 echo "Done updating permissions."
 
-
 if [ ! -f /config/config.php ]; then
     # New installation, run the setup
+    echo
+    echo "Detecting new install - generating config"
+    echo
     /usr/local/bin/setup.sh
 else
     # Run upgrade if applicable
+    echo
+    echo "Running occ upgrade"
     occ upgrade
 
     # Add missing indexes
+    echo
+    echo "Running occ db:add-missing-indices"
     occ db:add-missing-indices
 
     # Convert filecache fields
+    echo
+    echo "Running occ db:convert-filecache-bigint"
     occ db:convert-filecache-bigint
 
     # Update DB schema as needed
+    echo
+    echo "Running occ db:convert-mysql-charset"
     occ db:convert-mysql-charset
 
 fi
 
 # Run auto update
 if [ "$APP_AUTO_UPDATE" = "1" ] ; then
-  occ app:update --all
+    echo
+    echo "Running occ app:update --all"
+    occ app:update --all
 fi
 
+echo
+echo "Provisioning complete - starting app"
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
+echo
+echo "App exiting now"
+echo
